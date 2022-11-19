160fc69042c60d5afde84ceecf77ee2afdd6432d
Raphael Rouiller <rrouille@c1r7s8.42lausanne.ch>
2022-11-19 12:43:36 +0100

    Printf working, norminette needed

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,21 +6,17 @@
 /*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 15:35:04 by rrouille          #+#    #+#             */
-/*   Updated: 2022/11/15 11:27:42 by rrouille         ###   ########.fr       */
+/*   Updated: 2022/11/19 12:42:41 by rrouille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include "mylib.h"
 # include <stdarg.h>
 # include <unistd.h>
 
 int		ft_printf(const char *str, ...);
-void	ft_printstr(char *numero);
-void	ft_putunsigned(unsigned int num);
-void	ft_puthexa(unsigned int num, const char conversion);
-void	ft_printnbr(int num);
-void	ft_conversion(va_list args, const char conversion);
 
 #endif
--- srcs/printf.c
+++ srcs/printf.c
@@ -6,21 +6,86 @@
 /*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 15:34:59 by rrouille          #+#    #+#             */
-/*   Updated: 2022/11/15 11:26:53 by rrouille         ###   ########.fr       */
+/*   Updated: 2022/11/19 12:43:06 by rrouille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
-#include "mylib.h"
+/**
+* @author	rrouille
+* @title	Printf
+* @notice	This project is clear and efficient. You have to recode printf. You
+*			will then be able to reuse it in your future projects. This project
+*			is mainly about variable size arguments.
+*/
 
-void	ft_puthexa(unsigned int num, const char conversion)
+#include "ft_printf.h"
+
+/*
+ft_putunsigned
+print_str
+ft_printnbr
+ptr_len
+put_ptr
+ft_printptr
+charlen
+ft_conversion
+ft_printf*/
+
+/**
+* @notice	This function prints a string.
+* @param	char_pointer		The string to print.
+* @return	int					The lenght of characters of the printed
+*								string.
+*/
+int	print_str(char *str)
 {
-	if (num == 0)
-		ft_putchar_fd('0', 1);
-	else if (num >= 16)
+	int	len;
+
+	len = 0;
+	if (str == NULL)
 	{
-		ft_puthexa(num / 16, conversion);
-		ft_puthexa(num % 16, conversion);
+		ft_putstr_fd("(null)", 1);
+		return (6);
+	}
+	while (str[len])
+		ft_putchar_fd(str[len++], 1);
+	return (len);
+}
+
+/**
+* @notice	This function counts the lenght of an unsigned integer number converted
+*			into a hexadecimal.
+* @param	unsigned_int		The unsigned integer number to convert in
+*								hexadecimal.
+* @return	int					The lenght of characters of the hexadecimal number.
+*/
+int	hex_len(unsigned int num)
+{
+	int	len;
+
+	len = 0;
+	while (num != 0)
+	{
+		num /= 16;
+		len++;
+	}
+	return (len);
+}
+
+/**
+* @notice	This function converts an unsigned integer number into hexadecimal.
+* @param	unsigned_int		The unsigned integer number to convert in
+*								hexadecimal.
+* @param	const_char			The conversion needed ('x' for lowercase and 'X'
+*								for uppercase).
+* @return	void				No return value needed now.
+*/
+void	put_hex(unsigned int num, const char conversion)
+{
+	if (num >= 16)
+	{
+		put_hex(num / 16, conversion);
+		put_hex(num % 16, conversion);
 	}
 	else
 	{
@@ -36,105 +101,245 @@ void	ft_puthexa(unsigned int num, const char conversion)
 	}
 }
 
-void	ft_putunsigned(unsigned int num)
+/**
+* @notice	This function checks an unsigned integer number, converts it into
+*			hexadecimal, prints it and counts its lenght.
+* @param	unsigned_int		The unsigned integer number to convert in
+*								hexadecimal.
+* @param	const_char			The conversion needed ('x' for lowercase and 'X'
+*								for uppercase).
+* @return	int					The lenght of characters of the hexadecimal number.
+*/
+int	print_hex(unsigned int num, const char conversion)
 {
-	char	*numero;
+	if (num == 0)
+	{
+		ft_putchar_fd('0', 1);
+		return (1);
+	}
+	else
+		put_hex(num, conversion);
+	return (hex_len(num));
+}
+
+/**
+* @notice	This function counts the lenght of a integer number.
+* @param	unsigned_int		The unsigned integer number to count lenght.
+* @return	int					The lenght of characters of the number.
+*/
+int	num_len(unsigned int num)
+{
+	int	len;
+
+	len = 0;
+	while (num != 0)
+	{
+		num /= 10;
+		len++;
+	}
+	return (len);
+}
+
+/**
+* @notice	This function converts an unsigned interger number to a
+*			string.
+* @param	unsigned_int		The unsigned integer number to convert.
+* @return	string				The converted number.
+*/
+char	*unsigned_itoa(unsigned int num)
+{
+	char	*numchar;
 	int		len;
 
+	len = num_len(num);
+	numchar = (char *)malloc(sizeof(char) * (len + 1));
+	if (!numchar)
+		return (NULL);
+	numchar[len] = '\0';
+	while (num != 0)
+	{
+		numchar[len - 1] = num % 10 + '0';
+		num /= 10;
+		len--;
+	}
+	return (numchar);
+}
+
+/**
+* @notice	This function prints the unsigned interger number.
+* @param	unsigned_int		The unsigned integer number to print.
+* @return	int					The lenght of characters of the printed
+*								unsigned integer.
+*/
+int	print_unsigned(unsigned int num)
+{
+	int		len;
+	char	*numchar;
+
+	len = 0;
 	if (num == 0)
+	{
 		ft_putchar_fd('0', 1);
+		len++;
+	}
 	else
 	{
-		len = 0;
-		while (num != 0)
-		{
-			len++;
-			num = num / 10;
-		}
-		numero = (char *)malloc(sizeof(char) * (len + 1));
-		if (!numero)
-			;
-		numero[len] = '\0';
-		while (num != 0)
-		{
-			numero[len - 1] = num % 10 + 48;
-			num = num / 10;
-			len--;
-		}
-		ft_printstr(numero);
-		free(numero);
+		numchar = unsigned_itoa(num);
+		len += print_str(numchar);
+		free(numchar);
 	}
+	return (len);
 }
 
-void	ft_printstr(char *numero)
+/**
+* @notice	This function prints a number.
+* @param	int					The number to print.
+* @return	int					The lenght of characters of the printed
+*								number.
+*/
+int	print_nbr(int num)
 {
-	int	i;
+	int		len;
+	char	*numchar;
 
-	i = 0;
-	if (numero == NULL)
-		ft_putstr_fd("(null)", 1);
-	while (numero[i])
+	len = 0;
+	numchar = ft_itoa(num);
+	len = print_str(numchar);
+	free(numchar);
+	return (len);
+}
+
+/**
+* @notice	This function counts the lenght of a pointer.
+* @param	uintptr_t			The unsigned integer pointer to count lenght.
+* @return	int					The lenght of characters of the pointer.
+*/
+int	ptr_len(uintptr_t num)
+{
+	int	len;
+
+	len = 0;
+	while (num != 0)
 	{
-		write(1, &numero[i], 1);
-		i++;
+		len++;
+		num = num / 16;
+	}
+	return (len);
+}
+
+/**
+* @notice	This function converts a pointer.
+* @param	uintptr_t			The unsigned integer pointer to print.
+* @return	void				No return value needed now.
+*/
+void	put_ptr(uintptr_t num)
+{
+	if (num >= 16)
+	{
+		put_ptr(num / 16);
+		put_ptr(num % 16);
+	}
+	else
+	{
+		if (num <= 9)
+			ft_putchar_fd((num + '0'), 1);
+		else
+			ft_putchar_fd((num - 10 + 'a'), 1);
 	}
 }
 
-void	ft_printnbr(int num)
+/**
+* @notice	This function prints the pointer of something.
+* @param	unsigned_long_long	The unsigned integer pointer to print.
+* @return	int					The lenght of characters of the printed
+*								pointer.
+*/
+int	print_ptr(unsigned long long ptr)
 {
-	char	*numero;
+	int	len;
 
-	numero = ft_itoa(num);
-	ft_printstr(numero);
-	free(numero);
+	len = 2;
+	ft_putstr_fd("0x", 1);
+	if (ptr == 0)
+	{
+		ft_putchar_fd('0', 1);
+		len++;
+	}
+	else
+	{
+		put_ptr(ptr);
+		len += ptr_len(ptr);
+	}
+	return (len);
 }
 
-
-void	ft_conversion(va_list args, const char conversion)
+/**
+* @notice	This function links and converts the character next to '%' to the
+*			correct argument and function.
+* @param	va_list				The arguments.
+* @param	const_char			The character next to '%'.
+* @return	int					The lenght of characters of the printed
+*								conversion.
+*/
+int	arg_conversion(va_list args, const char conversion)
 {
+	int	len;
+
+	len = 1;
 	if (conversion == 'c')
 		ft_putchar_fd(va_arg(args, int), 1);
 	else if (conversion == 's')
-		ft_putstr_fd(va_arg(args, char *), 1);
+		len = print_str(va_arg(args, char *));
 	else if (conversion == 'p')
-		ft_putstr_fd(va_arg(args, void *), 1);
-	else if (conversion == 'd')
-		ft_printnbr(va_arg(args, int));
-	else if (conversion == 'i')
-		ft_printnbr(va_arg(args, int));
+		len = print_ptr(va_arg(args, unsigned long long));
+	else if (conversion == 'd' || conversion == 'i')
+		len = print_nbr(va_arg(args, int));
 	else if (conversion == 'u')
-		ft_putunsigned(va_arg(args, unsigned int));
+		len = print_unsigned(va_arg(args, unsigned int));
 	else if (conversion == 'x' || conversion == 'X')
-		ft_puthexa(va_arg(args, unsigned int), conversion);
+		len = print_hex(va_arg(args, unsigned int), conversion);
 	else if (conversion == '%')
-		ft_putchar_fd('%', 1);
+		ft_putstr_fd("%", 1);
+	return (len);
 }
 
+/**
+* @notice	The printf utility conversions and prints its arguments, after the
+*			first, under control of the conversion. The conversion is a character
+*			string which contains three types of objects: plain characters, which
+*			are simply copied to standard output, character escape sequences
+*			which are converted and copied to the standard output, and conversion
+*			specifications, each of which causes printing of the next successive
+*			argument.
+* @param	const_char			The arguments are treated as strings
+* @param	...					A variadic argument corresponding to each character
+*								after the '%' if there is some.
+* @return	int					The string lenght of all arguments.
+*/
 int	ft_printf(const char *str, ...)
 {
 	int		i;
-	int		j;
 	int		len;
 	va_list	args;
 
 	i = 0;
+	len = 0;
+	va_start(args, str);
 	while (str[i])
 	{
-		len = 0;
-		j = i + 1;
 		if (str[i] == '%')
 		{
-			len++;
-			ft_conversion(args, str[j]);
+			len += arg_conversion(args, str[i + 1]);
 			i++;
 		}
 		else
 		{
+			ft_putchar_fd(str[i], 1);
 			len++;
-			ft_putchar_fd(str[j], 1);
 		}
 		i++;
 	}
 	va_end(args);
 	return (len);
 }
+
